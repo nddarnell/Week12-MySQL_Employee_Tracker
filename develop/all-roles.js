7cf6ef35de0277e35ec522fd604db457ad0a0e7e
Nathan Darnell <darnell.nathaniel95@gmail.com>
2020-05-18 18:33:54 -0700

    finished code for viewing all roles

--- develop/all-roles.js
+++ develop/all-roles.js
@@ -2,24 +2,30 @@ const mysql = require("mysql")
 
 var connection = mysql.createConnection({
     host: "localhost",
-  
+
     // Your port; if not 3306
     port: 3306,
-  
+
     // Your username
     user: "root",
-  
+
     // Your password
     password: "SummerFalls13",
     database: "employeeListManager_db"
-  });
+});
+
+
 
 var roles = {
-    allRoles: function (){
+    allRoles: function () {
         // need to place database list of all roles here
         console.log("list of all roles here")
-        //connection.connection items here see activity 11 connection.query
-        //
+
+        connection.query("SELECT title FROM list_of_employees WHERE title IS NOT NULL GROUP BY title", function (err, roleResult) {
+            if (err) throw (err)
+            console.table(roleResult)
+        })
+
 
     }
 }
--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE employeeListManager_db;
 
-CREATE TABLE list(
+CREATE TABLE list_of_employees(
 	id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(40) NOT NULL,
     last_name VARCHAR(40) NOT NULL,
@@ -11,3 +11,17 @@ CREATE TABLE list(
     
     primary key (id)
 );
+
+INSERT INTO list_of_employees(first_name, last_name, title, department, salary, manager)
+VALUES ("Joe", "Nobody", "Salesman", "Sales", 75000, "Alex Bourne"), ("Larry", "Smith", "Salesman", "Sales", 75000, "Alex Bourne"),
+("Alex", "Bourne", "Sales Lead", "Sales", 90000, "Noel Miller"), ("Cody", "Prine", "3D Modeler", "Design", 70000, "Eric Saperstine"),
+("Pat", "Igore", "3D Modeler", "Design", 70000, "Eric Saperstine"), ("Eric", "Saperstine", "Design Lead", "Design", 90000, "Noel Miller"),
+("Patti", "Dole", "HR Rep", "Human Resources", 110000, "Noel Miller"), ("Melissa", "Drake", "Payroll Rep", "Payroll", 55000, "Noel Miller"),
+("Noel", "Miller", "CEO", "Board of Directors", 250000, "Share Holders");
+
+select * from list_of_employees;
+-- view all roles
+select title from list_of_employees 
+where title is not null
+group by title;
+
